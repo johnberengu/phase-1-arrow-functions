544d1574a54621a4233605f72ffab5e61889313b
eli <john.berengu@student.moringaschool.com>
2024-12-01 03:02:36 +0300

    completed

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = () => {
+const div= 2000/100;
+return div
+};
+const square = (x) => {
+return x*x
+};
+const add = (a,b) => {
+return a+b
+};
+console.log(square(2));
+console.log(add(3, 4))
+
+
+
